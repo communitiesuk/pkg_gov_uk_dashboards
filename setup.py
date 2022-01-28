890e9c53d8e62ee0c1b59a76aa059c7026eca81b
Kate Riley <kate.riley@communities.gov.uk>
2022-01-28 16:49:15 +0000

    added packages to setup

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     author="Department for Levelling Up, Housing and Communities",
     description="Provides Gov.UK colours in a class for convenient use, see: https://design-system.service.gov.uk/styles/colour/",
     name="GOV_UK_Colours",
+    packages=['GOV_UK_Colours', 'GOV_UK_Colours.*'],
     version="0.0.1",
 )
