e43df15e37adc868283e949bc919c1c25f0f9d22
Matthew Sutton <matthew.sutton@communities.gov.uk>
2022-02-02 14:03:34 +0000

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     description="Provides Gov.UK colours in a class for convenient use, see: https://design-system.service.gov.uk/styles/colour/",
     name="GOV_UK_Colours",
     packages=['GOV_UK_Colours'],
-    version="0.0.1",
+    version="1.0.0",
 )
