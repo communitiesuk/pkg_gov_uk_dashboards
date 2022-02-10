5ebe70c1946c64f6d491b8d8b1384e8d63fbae0c
Adrian Clay <adrian.clay@levellingup.gov.uk>
2022-02-10 16:36:44 +0000

    Create patch version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Department for Levelling Up, Housing and Communities",
     description="Provides access to functionality common to creating a data dashboard.",
     name="gov_uk_dashboards",
-    version="2.2.0",
+    version="2.2.1",
     packages=find_packages(),
     install_requires=[
         "setuptools~=59.8.0",
