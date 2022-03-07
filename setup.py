a905198b5eab3bf5754066ca003397d4046135be
Robert Blincoe <robert.blincoe@communities.gov.uk>
2022-03-07 13:57:37 +0000

    update package number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Department for Levelling Up, Housing and Communities",
     description="Provides access to functionality common to creating a data dashboard.",
     name="gov_uk_dashboards",
-    version="2.4.0",
+    version="2.6.0",
     packages=find_packages(),
     install_requires=[
         "setuptools~=59.8.0",
