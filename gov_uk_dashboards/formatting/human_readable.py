31ca435b367cc9e7261bc6d0b2cf602e12b9558d
Matthew Sutton <matthew.sutton@communities.gov.uk>
2022-04-08 10:54:16 +0100

    Added rounding functionality to formatting

--- gov_uk_dashboards/formatting/human_readable.py
+++ gov_uk_dashboards/formatting/human_readable.py
@@ -1,23 +1,46 @@
 """Functions for converting values to human readable format"""
 
 
-def format_as_human_readable(value_to_format: float, *, prefix: str = "") -> str:
-    """Format a number as a human readable string
-    Appends bn, m, k as appropriate
-    Rounds number to 3dp
+from typing import Optional
+
+
+def format_as_human_readable(
+    value_to_format: float,
+    *,
+    prefix: str = "",
+    suffix: str = "",
+    decimal_places: Optional[int] = None,
+) -> str:
+    """
+    Format a number as a human readable string.
+    Appends bn, m, k as appropriate.
+    E.g. 1,200,000 -> 1.2m
 
     Args:
         value_to_format(float): number to format
-        prefix(str,optional): prefix to append to the start
+        prefix(str,optional): prefix to append to the start (e.g. £). Defaults to no prefix.
+        suffix(str,optional): suffix to append to the end (e.g. %). Defaults to no suffix.
+        decimal_places(int, optional): If set, rounds formatted number to that many d.p.
+            This is applied after shortening, e.g. 1,234,000 with decimal places = 1 becomes
+            1.2m. Defaults to None, which does not apply rounding.
 
     Returns:
         str: formatted number
     """
+    value_suffix = ""
+    value = value_to_format
 
     if value_to_format >= 1_000_000_000:
-        return f"{prefix}{value_to_format/1_000_000_000:g}bn"
-    if value_to_format >= 1_000_000:
-        return f"{prefix}{value_to_format/1_000_000:g}m"
-    if value_to_format >= 1_000:
-        return f"{prefix}{value_to_format/1_000:g}k"
-    return f"{prefix}{value_to_format:g}"
+        value = value_to_format / 1_000_000_000
+        value_suffix = "bn"
+    elif value_to_format >= 1_000_000:
+        value = value_to_format / 1_000_000
+        value_suffix = "m"
+    elif value_to_format >= 1_000:
+        value = value_to_format / 1_000
+        value_suffix = "k"
+
+    if decimal_places:
+        value = round(value, decimal_places)
+
+    return f"{prefix}{value:g}{value_suffix}{suffix}"
