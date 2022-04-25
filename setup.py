d22aace4d8b27c13138eea35f2c95b4d43667d07
Matthew Sutton <matthew.sutton@communities.gov.uk>
2022-04-25 10:07:25 +0100

    Correct version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Department for Levelling Up, Housing and Communities",
     description="Provides access to functionality common to creating a data dashboard.",
     name="gov_uk_dashboards",
-    version="2.16.1",
+    version="2.16.0",
     packages=find_packages(),
     install_requires=[
         "setuptools~=59.8.0",
