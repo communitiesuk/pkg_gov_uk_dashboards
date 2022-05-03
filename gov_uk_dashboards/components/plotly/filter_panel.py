3891ccc829d26aa51eff57452f07c9ba239512b4
Jordan Hall <jordan@libertyware.co.uk>
2022-05-03 13:59:11 +0100

    feat: changed button color for user accesbility
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

--- gov_uk_dashboards/components/plotly/filter_panel.py
+++ gov_uk_dashboards/components/plotly/filter_panel.py
@@ -23,7 +23,7 @@ def filter_panel(children):
                 children=[
                     *children,
                     html.A(
-                        "Reset", className="govuk-button govuk-!-margin-0", href="?"
+                        "Reset", className="govuk-button govuk-button--warning govuk-!-margin-0", href="?"
                     ),
                 ],
             ),
