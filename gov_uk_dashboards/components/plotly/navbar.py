e423937e7d7c161d2fb8e2df9b6cd55d86731c9a
Jordan Hall <jordan@libertyware.co.uk>
2022-05-03 14:02:05 +0100

    fix remove comment
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

--- gov_uk_dashboards/components/plotly/navbar.py
+++ gov_uk_dashboards/components/plotly/navbar.py
@@ -2,36 +2,6 @@
 
 from dash import html, dcc
 
-# <div class = "moj-primary-navigation" >
-
-#  <div class = "moj-primary-navigation__container" >
-
-#    <div class = "moj-primary-navigation__nav" >
-
-#       <nav class = "moj-primary-navigation" aria-label = "Primary navigation" >
-
-#        <ul class = "moj-primary-navigation__list" >
-#          <li class = "moj-primary-navigation__item" >
-#             <a class = "moj-primary-navigation__link" aria-current = "page" href = "#1" > Nav item 1 < /a >
-#           </li >
-
-#           <li class = "moj-primary-navigation__item" >
-#             <a class = "moj-primary-navigation__link" href = "#2">Nav item 2</a>
-#           </li >
-
-#           <li class = "moj-primary-navigation__item" >
-#             <a class = "moj-primary-navigation__link" href = "#3">Nav item 3</a>
-#           </li >
-#         </ul >
-
-#       </nav >
-
-#     </div >
-#   </div >
-
-# </div >
-
-
 def navbar(links):
     """A navigation bar for switching between dashboards."""
     return html.Nav(
