2dce49a6def5071950a22fa51c1e2d963ba5e25a
Chris <chris.hopkins@madetech.com>
2022-05-05 16:46:05 +0100

    Added docstrings

--- gov_uk_dashboards/components/plotly/paragraph.py
+++ gov_uk_dashboards/components/plotly/paragraph.py
@@ -1,14 +1,17 @@
+"""paragraph component"""
 from enum import Enum
 from dash import html
 
 
 class ParagraphSizes(str, Enum):
+    """Enum class to specify the allowed paragraph sizes"""
     LEAD = "govuk-body-l"
     DEFAULT = "govuk-body"
     SMALL = "govuk-body-s"
 
 
 def paragraph(text: str, size: ParagraphSizes = ParagraphSizes.DEFAULT) -> html.P:
+    """Create a <p> Html component with the test provided"""
     if size not in list(ParagraphSizes):
         raise ValueError(
             f"Size {size} is not a valid paragraph size (use ParagraphSize enum)."
