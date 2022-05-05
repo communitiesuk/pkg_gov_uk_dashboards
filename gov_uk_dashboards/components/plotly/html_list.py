92b1e06772457a6a40a1f1dcec18095e6d2f8b2c
Chris <chris.hopkins@madetech.com>
2022-05-05 16:53:37 +0100

    Added the html list component

---
+++ gov_uk_dashboards/components/plotly/html_list.py
@@ -0,0 +1,22 @@
+"""html list component"""
+from dash import html
+
+
+def html_list(
+    list_items: list[str], *, numbered_list: bool = False, extra_spacing: bool = False
+):
+    """Create either a <ul> or <ol> component, with the children set as a list of <li>
+    components matching the list_items provided"""
+    li_items = [html.Li(x) for x in list_items]
+
+    classes = ["govuk-list"]
+    if extra_spacing:
+        classes.append("govuk-list--spaced")
+
+    if numbered_list:
+
+        classes.append("govuk-list--number")
+        return html.Ol(li_items, className=" ".join(classes))
+
+    classes.append("govuk-list--bullet")
+    return html.Ul(li_items, className=" ".join(classes))
