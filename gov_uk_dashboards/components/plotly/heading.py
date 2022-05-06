760abeb07e72d2f06c35656615785fd92d8d2acc
Chris <chris.hopkins@madetech.com>
2022-05-06 11:42:08 +0100

    pylint fixes

--- gov_uk_dashboards/components/plotly/heading.py
+++ gov_uk_dashboards/components/plotly/heading.py
@@ -14,12 +14,17 @@ class HeadingSizes(str, Enum):
 
 
 def heading1(text: str, size: HeadingSizes = HeadingSizes.LARGE) -> html.H1:
+    """Return a H1 dash component"""
     return html.H1(text, className=size)
 
 
 def heading2(text: str, size: HeadingSizes = HeadingSizes.MEDIUM) -> html.H2:
+    """Return a H2 dash component"""
+
     return html.H2(text, className=size)
 
 
 def heading3(text: str, size: HeadingSizes = HeadingSizes.SMALL) -> html.H3:
+    """Return a H3 dash component"""
+
     return html.H3(text, className=size)
