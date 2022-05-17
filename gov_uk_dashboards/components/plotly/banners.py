406b64ca79961c1d67f5543b440f1651ab99baef
Jordan Hall <jordan@libertyware.co.uk>
2022-05-17 13:09:49 +0100

    lint: repair linting
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

--- gov_uk_dashboards/components/plotly/banners.py
+++ gov_uk_dashboards/components/plotly/banners.py
@@ -10,21 +10,21 @@ def message_banner(category, message):
     See https://coronavirus.data.gov.uk
     """
     return html.Div(
-            html.P(
-                [
-                    html.Strong(
-                        category,
-                        className="govuk-tag",
-                        style={
-                            "background": "white",
-                            "color": "#1d70b8",
-                            "margin": "0 1rem 0 0",
-                            "lineHeight": "initial",
-                        },
-                    ),
-                    message,
-                ],
-                className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-bottom-0",
-            ),
-            className="change-log-banner govuk-!-margin-bottom-2",
-        )
+        html.P(
+            [
+                html.Strong(
+                    category,
+                    className="govuk-tag",
+                    style={
+                        "background": "white",
+                        "color": "#1d70b8",
+                        "margin": "0 1rem 0 0",
+                        "lineHeight": "initial",
+                    },
+                ),
+                message,
+            ],
+            className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-bottom-0",
+        ),
+        className="change-log-banner govuk-!-margin-bottom-2",
+    )
