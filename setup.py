b52a1bd2e956225f60626396432083fa9e32c4e6
Matthew Sutton <matthew.sutton@communities.gov.uk>
2022-05-20 16:55:29 +0100

    Fix missing plotly requirement from dependencies
    Plotly is required for the line figure but was not in the dependencies.
    Added to environment.yml, requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "numpy>=1.22.0",
         "dash_bootstrap_components~=1.1.0",
         "pandas>=1.3",
+        "plotly~=5.5.0",
     ],
     package_data={"": ["gov_uk_dashboards/template.html"]},
     include_package_data=True,
