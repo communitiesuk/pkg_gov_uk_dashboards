2204f2b58bc5fd5ad3f2c4776eac2e3e62c3db09
Matthew Sutton <matthew.sutton@communities.gov.uk>
2022-05-20 17:29:17 +0100

    Add captioned_figure component to aid accessibility
    Provides a simple way to create a correctly structured html figure.
    This aids accessibility through providing text for a screen reader.

---
+++ gov_uk_dashboards/components/plotly/captioned_figure.py
@@ -0,0 +1,54 @@
+"""captioned_figure function"""
+from typing import Any, Optional, Union
+import plotly
+import dash
+import gov_uk_dashboards.components.plotly
+
+
+def captioned_figure(
+    figure: plotly.graph_objects.Figure,
+    captions: Union[
+        dash.development.base_component.Component,
+        list[dash.development.base_component.Component],
+    ],
+    figure_name: str,
+    style: Optional[dict[str, Any]] = None,
+):
+    """
+    Return figure with attached caption that can be read by a screen reader.
+
+    The caption will not be displayed to users viewing the website through a
+    browser but is available for the a screen reader to describe.
+
+    The supplied graph and caption are wrapped in an HTML <figure > element.
+
+    Args:
+        figure (plotly.graph_objects.Figure): The plotly figure to display and caption.
+        captions (Component, list[Component]): The captions to apply to the
+            figure to be read by a screen reader.
+        figure_name (str): Identifier for the figure. Should be lower case,
+            with words separated by dashes.
+        style (Optional[dict], optional): Any custom style to apply to the graph.
+            Defaults to None.
+
+    Returns:
+        dash.html.Figure: Figure html element containing the graph and its caption.
+    """
+    return dash.html.Figure(
+        [
+            dash.html.Div(
+                children=gov_uk_dashboards.components.plotly.graph(
+                    element_id=f"{figure_name}-figure",
+                    figure=figure,
+                    style=style,
+                ),
+                **{"role": "img", "aria-labelledby": f"{figure_name}-caption"},
+                id=f"{figure_name}-graph",
+            ),
+            dash.html.Figcaption(
+                children=captions,
+                id=f"{figure_name}-caption",
+                className="govuk-visually-hidden",
+            ),
+        ]
+    )
