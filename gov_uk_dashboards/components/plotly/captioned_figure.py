b6d463a7799f2a090b2f84d892c9e185ba5303fa
Matthew Sutton <matthew.sutton@communities.gov.uk>
2022-05-23 10:40:08 +0100

    Removed dependency on graph as will be deprecated.

--- gov_uk_dashboards/components/plotly/captioned_figure.py
+++ gov_uk_dashboards/components/plotly/captioned_figure.py
@@ -2,7 +2,6 @@
 from typing import Any, Optional, Union
 import plotly
 import dash
-from gov_uk_dashboards.components.plotly.graph import graph
 
 
 def captioned_figure(
@@ -12,7 +11,7 @@ def captioned_figure(
         list[dash.development.base_component.Component],
     ],
     figure_name: str,
-    style: Optional[dict[str, Any]] = None,
+    graph_style: Optional[dict[str, Any]] = None,
 ):
     """
     Return figure with attached caption that can be read by a screen reader.
@@ -27,23 +26,31 @@ def captioned_figure(
         captions (Component, list[Component]): The captions to apply to the
             figure to be read by a screen reader.
         figure_name (str): Identifier for the figure. Should be lower case,
-            with words separated by dashes.
-        style (Optional[dict], optional): Any custom style to apply to the graph.
+            with words separated by dashes, e.g. jitter-figure
+        graph_style (Optional[dict], optional): Any custom style to apply to the graph.
             Defaults to None.
 
     Returns:
         dash.html.Figure: Figure html element containing the graph and its caption.
     """
+    if not graph_style:
+        graph_style = {}
+
+    if "height" not in graph_style.keys():
+        graph_style["height"] = "450px"
+
     return dash.html.Figure(
         [
             dash.html.Div(
-                children=graph(
-                    element_id=f"{figure_name}-figure",
+                children=dash.dcc.Graph(
+                    id=f"{figure_name}-graph",
+                    responsive=True,
                     figure=figure,
-                    style=style,
+                    style=graph_style,
+                    config={"displayModeBar": False},
                 ),
                 **{"role": "img", "aria-labelledby": f"{figure_name}-caption"},
-                id=f"{figure_name}-graph",
+                id=f"{figure_name}-figure",
             ),
             dash.html.Figcaption(
                 children=captions,
