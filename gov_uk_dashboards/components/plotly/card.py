8e11169b2d22eb94e6424de788db34750860db1f
Jordan Hall <jordan@libertyware.co.uk>
2022-05-24 23:15:31 +0100

    fix: row and card should always be a div
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

--- gov_uk_dashboards/components/plotly/card.py
+++ gov_uk_dashboards/components/plotly/card.py
@@ -5,7 +5,7 @@ from dash import html
 def card(children):
     """A rectangle with a grey background.
     Mostly used to wrap individual visualisations, e.g. a gauge."""
-    return html.Li(children, className="mini-card")
+    return html.Div(children, className="mini-card")
 
 
 def empty_card():
