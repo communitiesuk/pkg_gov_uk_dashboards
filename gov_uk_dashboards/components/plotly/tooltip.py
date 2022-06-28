16f52738556d02eb53bd28f424a478e13a1459b9
Kate Riley <kate.riley@communities.gov.uk>
2022-06-28 15:56:35 +0000

    pass style attributes as parameters

--- gov_uk_dashboards/components/plotly/tooltip.py
+++ gov_uk_dashboards/components/plotly/tooltip.py
@@ -1,14 +1,18 @@
 """tooltip"""
+from typing import Optional
 from dash import html
 import dash_bootstrap_components as dbc
 
 
-def tooltip(tooltip_text: str, tooltip_id: str):
+def tooltip(tooltip_text: str, tooltip_id: str, color: str, font_size: int, width: Optional (str)):
     """Creates a tooltip for providing further information
 
     Args:
         tooltip_text (str): The text to display when hovering
         tooltip_id (str): The element ID to contain the tooltip text
+        color (str): The color of the tooltip text
+        font_size (int): The size of the tooltip text
+        width (str): The width of the element containing the tooltip text
 
     Returns:
         html.Span: The dash HTML object for the tooltip.
@@ -21,7 +25,7 @@ def tooltip(tooltip_text: str, tooltip_id: str):
                 tooltip_text,
                 target=tooltip_id,
                 class_name="tooltip tooltiptext",
-                style={"color": "white", "font-size": 12, "width": "10px"},
+                style={"color": color, "font-size": font_size, "width": width},
             ),
         ],
         id=tooltip_id,
