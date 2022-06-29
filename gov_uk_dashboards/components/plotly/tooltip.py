c755bfa1e65815be7ddd3d8e225087d5e59766fc
Kate Riley <kate.riley@communities.gov.uk>
2022-06-29 13:55:10 +0000

    update arguments to tooltip function

--- gov_uk_dashboards/components/plotly/tooltip.py
+++ gov_uk_dashboards/components/plotly/tooltip.py
@@ -3,14 +3,15 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 
-def tooltip(tooltip_text: str, tooltip_id: str, color: str, font_size: int):
+def tooltip(tooltip_text: str, tooltip_id: str, color: str="white", font_size: str = "16px", width: 
+    str = "10px"):
     """Creates a tooltip for providing further information
 
     Args:
         tooltip_text (str): The text to display when hovering
         tooltip_id (str): The element ID to contain the tooltip text
         color (str): The color of the tooltip text
-        font_size (int): The size of the tooltip text
+        font_size (str): The size of the tooltip text
 
     Returns:
         html.Span: The dash HTML object for the tooltip.
@@ -23,7 +24,7 @@ def tooltip(tooltip_text: str, tooltip_id: str, color: str, font_size: int):
                 tooltip_text,
                 target=tooltip_id,
                 class_name="tooltip tooltiptext",
-                style={"color": color, "font-size": font_size},
+                style={"color": color, "font-size": font_size, "width": width},
             ),
         ],
         id=tooltip_id,
