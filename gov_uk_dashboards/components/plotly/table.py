8f418555bece4a310d4769bbc0016d573bd2f28c
tom.clarke <tom.clarke@levellingup.gov.uk>
2022-08-03 10:06:18 +0100

    ran black

--- gov_uk_dashboards/components/plotly/table.py
+++ gov_uk_dashboards/components/plotly/table.py
@@ -10,8 +10,7 @@ def table_from_dataframe(
     include_headers: bool = True,
     first_column_is_header: bool = True,
     title_is_subtitle: bool = False,
-    short_table: bool = False
-    **table_properties
+    short_table: bool = False**table_properties,
 ):
     """
     Displays a pandas DataFrame as a table formatted in the Gov.UK style
@@ -57,7 +56,9 @@ def table_from_dataframe(
                     ],
                     className="govuk-table__row",
                 ),
-                className="govuk-table__head-short" if short_table else "govuk-table__head",
+                className="govuk-table__head-short"
+                if short_table
+                else "govuk-table__head",
             )
         )
 
