15ede41d82e703e8c4207a7754cb3778754985e8
robertblincoe <robert.blincoe@levellingup.gov.uk>
2022-10-25 16:26:44 +0100

    four tests to check key value pairs can handle string, float, none, nan

---
+++ tests/unit_tests/gov_uk_dashboards/components/plotly/test_key_value_pair.py
@@ -0,0 +1,35 @@
+"""Tests for key_value_pair component"""
+from cmath import nan
+import pytest
+from dash import html
+
+from gov_uk_dashboards.components.plotly.key_value_pair import key_value_pair
+
+
+def test_key_value_pair_works_for_float():
+    """Test component returns an array with a <Dt> and <Dd> for a float value input"""
+    actual = key_value_pair("a", 1.0)
+
+    assert actual[1].children == 1.0
+    
+    
+def test_key_value_pair_works_for_string():
+    """Test component returns an array with a <Dt> and <Dd> for a string value input"""
+    actual = key_value_pair("a", "hat")
+
+    assert actual[1].children == "hat"
+
+
+def test_key_value_pair_works_for_none():
+    """Test component returns an array with a <Dt> and <Dd> for a none value input"""
+    actual = key_value_pair("a", None)
+
+    assert actual[1].children == "-"
+    
+    
+def test_key_value_pair_works_for_nan():
+    """Test component returns an array with a <Dt> and <Dd> for a nan value input"""
+    actual = key_value_pair("a", float("Nan"))
+
+    assert actual[1].children == "-"
+
