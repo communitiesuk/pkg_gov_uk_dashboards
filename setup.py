e482883b36f1db7332c25ee5312efd2ef26fa320
Kate Riley <kate.riley@communities.gov.uk>
2022-10-26 15:00:05 +0100

    update govuk-font-family style

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Department for Levelling Up, Housing and Communities",
     description="Provides access to functionality common to creating a data dashboard.",
     name="gov_uk_dashboards",
-    version="6.4.5",
+    version="6.4.6",
     packages=find_packages(),
     install_requires=[
         "setuptools~=59.8",
