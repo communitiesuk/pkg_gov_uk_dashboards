017a898ca8254c929c1b6a1627cf29c90d981976
robertblincoe <robert.blincoe@levellingup.gov.uk>
2023-01-04 16:28:16 +0000

    fix line lengths for pylnt

--- gov_uk_dashboards/symbols.py
+++ gov_uk_dashboards/symbols.py
@@ -2,14 +2,14 @@
 
 Contains:
 - Symbols from the Plotly graphing library
-https://plotly.com/python/marker-style/#:~:text=The%20basic%20symbols%20are%3A%20circle,hash%20%2C%20y%20%2C%20and%20line%20.
+https://plotly.com/python/marker-style/#custom-marker-symbols
 """
 from enum import Enum
 
 
 class PlotSymbols(Enum):
     """
-    From theplotly graphing library: https://plotly.com/python/marker-style/#:~:text=The%20basic%20symbols%20are%3A%20circle,hash%20%2C%20y%20%2C%20and%20line%20.
+    From theplotly graphing library: https://plotly.com/python/marker-style/#custom-marker-symbols
     """
 
     CIRCLE = "circle"
