93518efe576bfa312a7ed0d47722cc8e3ab3f141
tom.clarke <tom.clarke@levellingup.gov.uk>
2023-04-06 10:03:18 +0000

    Check for username and password in basic_auth function

--- gov_uk_dashboards/lib/enable_basic_auth.py
+++ gov_uk_dashboards/lib/enable_basic_auth.py
@@ -8,11 +8,14 @@ from flask_basicauth import BasicAuth
 def enable_basic_auth(dash_app: dash.Dash):
     """Turn on basic auth for the dash app if required"""
     if os.environ.get("STAGE") == "production":
-        server = dash_app.server
+        if os.environ.get("APP_USERNAME") and os.environ.get("APP_PASSWORD"):
+            server = dash_app.server
 
-        # Forcing the auth to prompt on any page if it hasn't already been done.
-        server.config["BASIC_AUTH_FORCE"] = True
+            # Forcing the auth to prompt on any page if it hasn't already been done.
+            server.config["BASIC_AUTH_FORCE"] = True
 
-        server.config["BASIC_AUTH_USERNAME"] = os.environ.get("APP_USERNAME")
-        server.config["BASIC_AUTH_PASSWORD"] = os.environ.get("APP_PASSWORD")
-        BasicAuth(server)
+            server.config["BASIC_AUTH_USERNAME"] = os.environ.get("APP_USERNAME")
+            server.config["BASIC_AUTH_PASSWORD"] = os.environ.get("APP_PASSWORD")
+            BasicAuth(server)
+        else:
+            print("failed to set up basic_auth")
