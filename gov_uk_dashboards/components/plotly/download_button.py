4bfd02a39b9049e95feecaccf2583c2c6079204b
Matt Pike <matt.pike@levellingup.gov.uk>
2023-06-15 16:05:35 +0100

    Add optional id variable to dropdown

--- gov_uk_dashboards/components/plotly/download_button.py
+++ gov_uk_dashboards/components/plotly/download_button.py
@@ -1,19 +1,21 @@
 """download_button"""
+from typing import Optional
 from dash import html
 
 
-def download_button(button_text: str):
+def download_button(button_text: str, id: Optional[str] = "download-button"):
     """
     Return a download button which is aligned to the right
 
     Args:
     button_text (str): The text to display on the button.
+    id: (str, Optional) = id for dropdown, default to "download-button"
     """
     return html.Div(
         [
             html.Button(
                 button_text,
-                id="download-button",
+                id=id,
                 n_clicks=0,
                 className="govuk-button govuk-button--secondary",
             ),
