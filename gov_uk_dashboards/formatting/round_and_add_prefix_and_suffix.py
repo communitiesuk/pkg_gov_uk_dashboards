787af61d9a1ef4926b453aef506a2572174313fb
Matt Pike <matt.pike@levellingup.gov.uk>
2023-06-29 11:16:00 +0100

    Add and fix unit test

--- gov_uk_dashboards/formatting/round_and_add_prefix_and_suffix.py
+++ gov_uk_dashboards/formatting/round_and_add_prefix_and_suffix.py
@@ -27,10 +27,10 @@ def round_and_add_prefix_and_suffix(
     if value is None or math.isnan(value):
         return separator
     if decimal_places is not None:
-        if decimal_places > 0:
-            value = round(value, decimal_places)
-            value = f"{value:.{decimal_places}f}"
-        else:
+        value = round(value, decimal_places)
+        if decimal_places == 0:
             value = round(value)
+        if decimal_places > 0:
+            value = f"{value:.{decimal_places}f}"
 
     return f"{prefix}{value}{suffix}"
