c10b0a4e515249b7c3e8dfa1e89c2a734b2ddca4
Ellie-Brakoniecki <112866399+Ellie-Brakoniecki@users.noreply.github.com>
2023-07-03 16:35:08 +0100

    button bottom margin

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="Department for Levelling Up, Housing and Communities",
     description="Provides access to functionality common to creating a data dashboard.",
     name="gov_uk_dashboards",
-    version="9.12.0",
+    version="9.13.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
