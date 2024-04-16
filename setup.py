83b70ae03fcaac6714b780bcf6755c8bb902a311
Kate Riley <kate.riley@communities.gov.uk>
2024-04-16 13:56:39 +0100

    wip

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="Department for Levelling Up, Housing and Communities",
     description="Provides access to functionality common to creating a data dashboard.",
     name="gov_uk_dashboards",
-    version="9.30.0",
+    version="9.30.1",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
