cf987b13957f152201e4f0eb3b218f1e2eea1b9f
Ellie-Brakoniecki <112866399+Ellie-Brakoniecki@users.noreply.github.com>
2024-04-26 09:09:20 +0100

    update style of download button

--- gov_uk_dashboards/components/plotly/download_button.py
+++ gov_uk_dashboards/components/plotly/download_button.py
@@ -1,23 +1,30 @@
 """download_button"""
-from typing import Optional
 from dash import html
 
 
-def download_button(button_text: str, button_id: Optional[str] = "download-button"):
+def download_button(button_text: str, button_id: str = "download-button"):
     """
-    Return a download button which is aligned to the right
+    Return a download button styled with additional classes and an SVG icon,
+    aligned to the right.
 
     Args:
     button_text (str): The text to display on the button.
-    button_id: (str, Optional) = id for dropdown, default to "download-button"
+    button_id (str, Optional): ID for the button, defaults to "download-button".
     """
+    classes = (
+        "govuk-button govuk-button--primary "
+        "govuk-!-margin-bottom-0 govuk-!-margin-top-4 "
+        "flex w-auto items-center gap-2 print:hidden"
+    )
     return html.Div(
         [
             html.Button(
-                button_text,
+                [html.Div("", className="download-icon"), button_text],
                 id=button_id,
                 n_clicks=0,
-                className="govuk-button govuk-button--secondary",
+                className=classes,
+                type="submit",
+                style={"display": "flex", "alignItems": "center", "gap": "8px"},
             ),
         ],
         className="govuk-button-group",
