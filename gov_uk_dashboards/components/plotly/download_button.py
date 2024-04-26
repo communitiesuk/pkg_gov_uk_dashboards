da0d03b3b5f3c478fede3582a340637093a8c2ee
Ellie-Brakoniecki <112866399+Ellie-Brakoniecki@users.noreply.github.com>
2024-04-26 11:10:58 +0100

    add new fn for download button

--- gov_uk_dashboards/components/plotly/download_button.py
+++ gov_uk_dashboards/components/plotly/download_button.py
@@ -1,8 +1,38 @@
 """download_button"""
+import warnings
 from dash import html
 
 
 def download_button(button_text: str, button_id: str = "download-button"):
+    """
+    Return a download button which is aligned to the right
+
+    Args:
+    button_text (str): The text to display on the button.
+    button_id: (str, Optional) = id for dropdown, default to "download-button"
+    """
+    warnings.warn(
+        "Note there is an alternative function to download_button() called "
+        "download_button_with_icon() which includes a download icon and improved styling.",
+        Warning,
+        stacklevel=2,
+    )
+
+    return html.Div(
+        [
+            html.Button(
+                button_text,
+                id=button_id,
+                n_clicks=0,
+                className="govuk-button govuk-button--secondary",
+            ),
+        ],
+        className="govuk-button-group",
+        style={"float": "right"},
+    )
+
+
+def download_button_with_icon(button_text: str, button_id: str = "download-button"):
     """
     Return a download button styled with additional classes and an SVG icon,
     aligned to the right.
