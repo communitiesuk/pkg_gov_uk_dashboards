95cd53cd5bfc98c8a997836fcf0985f57bf104d4
Kate Riley <kate.riley@communities.gov.uk>
2024-12-19 16:04:29 +0000

    run formatter

--- gov_uk_dashboards/components/plotly/table.py
+++ gov_uk_dashboards/components/plotly/table.py
@@ -4,6 +4,7 @@ from pandas import DataFrame
 from dash import html, dcc
 from gov_uk_dashboards.components.plotly.card import card
 from gov_uk_dashboards.components.plotly.paragraph import paragraph
+
 # from gov_uk_dashboards.components.plotly.row_component import row_component
 
 
