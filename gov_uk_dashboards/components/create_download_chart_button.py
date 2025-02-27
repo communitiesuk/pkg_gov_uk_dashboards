29a64b49923dc89ee8277663976d6454757202bc
James Coker <James.Coker@communities.gov.uk>
2025-02-27 15:13:03 +0000

    Add choropleth map

---
+++ gov_uk_dashboards/components/create_download_chart_button.py
@@ -0,0 +1,25 @@
+"""create_download_chart_button"""
+
+from dash import html
+from gov_uk_dashboards.components.plotly import download_button_with_icon
+
+
+def create_download_chart_button(button_id_name: str) -> html.Div:
+    """Create a download button for charts, aligned to the right.
+
+    Parameters:
+    - button_id_name (str): A unique identifier for the button.
+
+    Returns:
+    - html.Div: A Div element containing a download button.
+    """
+    return html.Div(
+        download_button_with_icon(
+            button_text="Download chart",
+            button_id={
+                "download-type": "download-chart",
+                "name": button_id_name,
+            },
+        ),
+        style={"textAlign": "right"},
+    )
