95011b3bc7a2477a0551d7c449784ca28d2b1783
James Coker <James.Coker@communities.gov.uk>
2025-08-06 11:33:51 +0100

    Add ids to headings

--- gov_uk_dashboards/components/dash/heading.py
+++ gov_uk_dashboards/components/dash/heading.py
@@ -16,16 +16,17 @@ class HeadingSizes(str, Enum):
 
 def heading1(text: str, size: HeadingSizes = HeadingSizes.LARGE) -> html.H1:
     """Return a H1 dash component"""
-    return html.H1(text, className=size)
+    heading_id = f"heading1-{"-".join(text.lower())}"
+    return html.H1(text, className=size, id=heading_id)
 
 
 def heading2(text: str, size: HeadingSizes = HeadingSizes.MEDIUM) -> html.H2:
     """Return a H2 dash component"""
-
-    return html.H2(text, className=size)
+    heading_id = f"heading2-{"-".join(text.lower())}"
+    return html.H2(text, className=size, id=heading_id)
 
 
 def heading3(text: str, size: HeadingSizes = HeadingSizes.SMALL) -> html.H3:
     """Return a H3 dash component"""
-
-    return html.H3(text, className=size)
+    heading_id = f"heading3-{"-".join(text.lower())}"
+    return html.H3(text, className=size, id=heading_id)
