7a186526d6f1f54e5197dd0873defc8ea9f25fb4
James Coker <James.Coker@communities.gov.uk>
2025-08-06 14:00:48 +0100

    Linting

--- gov_uk_dashboards/components/dash/main_content.py
+++ gov_uk_dashboards/components/dash/main_content.py
@@ -17,15 +17,16 @@ def main_content(children, id_fragment=None):
     See https://developer.mozilla.org/en-US/docs/Learn/Accessibility/HTML for more guidance.
     """
     if id_fragment:
-        parts = re.split(r'\W+', id_fragment.lower())
+        parts = re.split(r"\W+", id_fragment.lower())
         slug = "main-content-" + "-".join(filter(None, parts))
     else:
         slug = ""
         warnings.warn(
-            "WARNING: No id_fragment provided to main_content(). This is usually the page title and helps "
-            "create a unique and consistent id attribute. Providing an id_fragment improves accessibility, "
-            "aids automated testing, and prevents issues with duplicate or missing page headings.",
+            "WARNING: No id_fragment provided to main_content(). This is usually the page title and"
+            " helps create a unique and consistent id attribute. Providing an id_fragment improves "
+            "accessibility, aids automated testing, and prevents issues with duplicate or missing "
+            "page headings.",
             UserWarning,
-            stacklevel=2
+            stacklevel=2,
         )
     return html.Main(children, className="main", id=slug, role="main")
