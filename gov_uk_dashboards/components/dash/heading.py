6999ebf38733da242f12e7718a7ba582640dddcc
James Coker <James.Coker@communities.gov.uk>
2025-08-08 14:28:32 +0100

    Fix heading id creation

--- gov_uk_dashboards/components/dash/heading.py
+++ gov_uk_dashboards/components/dash/heading.py
@@ -3,6 +3,7 @@
 from enum import Enum
 
 from dash import html
+from gov_uk_dashboards.formatting.text_functions import create_id_from_string
 
 
 class HeadingSizes(str, Enum):
@@ -16,17 +17,17 @@ class HeadingSizes(str, Enum):
 
 def heading1(text: str, size: HeadingSizes = HeadingSizes.LARGE) -> html.H1:
     """Return a H1 dash component"""
-    heading_id = f"heading1-{"-".join(text.lower())}"
+    heading_id = f"heading1-{create_id_from_string(text)}"
     return html.H1(text, className=size, id=heading_id)
 
 
 def heading2(text: str, size: HeadingSizes = HeadingSizes.MEDIUM) -> html.H2:
     """Return a H2 dash component"""
-    heading_id = f"heading2-{"-".join(text.lower())}"
+    heading_id = f"heading2-{create_id_from_string(text)}"
     return html.H2(text, className=size, id=heading_id)
 
 
 def heading3(text: str, size: HeadingSizes = HeadingSizes.SMALL) -> html.H3:
     """Return a H3 dash component"""
-    heading_id = f"heading3-{"-".join(text.lower())}"
+    heading_id = f"heading3-{create_id_from_string(text)}"
     return html.H3(text, className=size, id=heading_id)
