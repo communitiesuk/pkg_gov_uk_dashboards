29f01699dd408a7ced5aed1fe40593f3f5cf1baf
James Coker <James.Coker@communities.gov.uk>
2025-08-28 16:13:51 +0100

    Black

--- gov_uk_dashboards/components/dash/header.py
+++ gov_uk_dashboards/components/dash/header.py
@@ -3,7 +3,12 @@
 from dash import html
 
 
-def header(title: str, strong_class: str = "", background_colour: str = None, show_mobile_menu_button: bool = True):
+def header(
+    title: str,
+    strong_class: str = "",
+    background_colour: str = None,
+    show_mobile_menu_button: bool = True,
+):
     """
     The header component, shared across all dashboard views. Includes optional strong_class
     parameter to provide extra styling in the form of a class.
@@ -52,11 +57,15 @@ def header(title: str, strong_class: str = "", background_colour: str = None, sh
                         ],
                         className=f"{strong_class}",
                     ),
-                    html.Button(
-                        "Menu ▼",
-                        id="mobile-menu-btn",
-                        className="mobile-menu-button govuk-button",
-                    ) if show_mobile_menu_button else None,
+                    (
+                        html.Button(
+                            "Menu ▼",
+                            id="mobile-menu-btn",
+                            className="mobile-menu-button govuk-button",
+                        )
+                        if show_mobile_menu_button
+                        else None
+                    ),
                 ],
                 className="govuk-header__content",
             ),
