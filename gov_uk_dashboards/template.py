1bb3adc96c87393def544fe29974edf9029d2d89
James Coker <James.Coker@communities.gov.uk>
2026-01-09 11:37:29 +0000

    Add app insights to template and csp

--- gov_uk_dashboards/template.py
+++ gov_uk_dashboards/template.py
@@ -13,6 +13,9 @@ def read_template(favicon_path: str = r"assets\images\MHCLG_favicon.png") -> str
     :return: String version of the template.
     """
     gtag = os.environ.get("GTAG", "")
+    app_insights_conn_string = os.environ.get(
+        "APPLICATIONINSIGHTS_CONNECTION_STRING", ""
+    )
     path = os.path.join(os.path.dirname(__file__), "template.html")
 
     with open(path, encoding="utf-8") as file:
@@ -20,4 +23,7 @@ def read_template(favicon_path: str = r"assets\images\MHCLG_favicon.png") -> str
 
         rendered_template = template.replace("{{favicon_path}}", favicon_path)
         rendered_template = rendered_template.replace("{{gtag}}", gtag)
+        rendered_template = rendered_template.replace(
+            "{{app_insights_conn_string}}", app_insights_conn_string
+        )
         return rendered_template
