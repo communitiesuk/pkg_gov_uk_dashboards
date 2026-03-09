fc9dcfad01c50e7224535ed462d73cc85d78eb43
Kate Riley <kate.riley@communities.gov.uk>
2026-03-09 11:27:58 +0000

    formatter

--- gov_uk_dashboards/components/plotly/time_series_chart.py
+++ gov_uk_dashboards/components/plotly/time_series_chart.py
@@ -375,9 +375,15 @@ class TimeSeriesChart:
 
         ticks = self._get_y_axis_ticks()
 
-        max_y_range = ticks[-2]+2*(ticks[-1]-ticks[-2])/3
-    
-        fig.update_yaxes(rangemode="tozero", showgrid=True, range=[0,max_y_range], tickvals=ticks, ticktext=[f"{v:,}" for v in ticks])  # formatted with commas)
+        max_y_range = ticks[-2] + 2 * (ticks[-1] - ticks[-2]) / 3
+
+        fig.update_yaxes(
+            rangemode="tozero",
+            showgrid=True,
+            range=[0, max_y_range],
+            tickvals=ticks,
+            ticktext=[f"{v:,}" for v in ticks],  # formatted with commas,
+        )
 
         update_layout_bgcolor_margin(fig, "#FFFFFF")
 
@@ -587,7 +593,7 @@ class TimeSeriesChart:
             total_tick_points = int((tick_text_length / 5) * 7)
             total_tick_points = min(
                 total_tick_points, tick_text_length + 3
-            )  # don't want more than quarter year extra ?
+            )  # don't want more than quarter year extra
             additional_tick_points = total_tick_points - tick_text_length
             last_current_tick_text = datetime.strptime(tick_text[-1], "%b %Y")
 
@@ -690,10 +696,10 @@ class TimeSeriesChart:
         else:
             largest_y_value = self.filtered_df[self.y_axis_column].max()
         y_axis_max = largest_y_value + (0.3 * largest_y_value)
-        
+
         # Generate nice ticks
         ticks = self.generate_nice_ticks(0, y_axis_max)
-        
+
         return ticks
 
     def _get_df_list_for_time_series(self) -> list[pl.DataFrame]:
@@ -803,51 +809,49 @@ class TimeSeriesChart:
         return [trace_colour[t] for t in self.trace_name_list]
 
     def generate_nice_ticks(self, y_min, y_max, max_ticks=10):
-                """
-                Generate "nice" tick values for a numeric axis.
+        """
+        Generate "nice" tick values for a numeric axis.
 
-                Args:
-                    y_min (float): Minimum axis value
-                    y_max (float): Maximum axis value
-                    max_ticks (int): Maximum number of ticks
+        Args:
+            y_min (float): Minimum axis value
+            y_max (float): Maximum axis value
+            max_ticks (int): Maximum number of ticks
 
-                Returns:
-                    list: Tick values (floats) that are rounded and evenly spaced
-                """
+        Returns:
+            list: Tick values (floats) that are rounded and evenly spaced
+        """
 
-                # Step 1: compute raw step
-                raw_step = (y_max - y_min) / (max_ticks - 1)
+        # Step 1: compute raw step
+        raw_step = (y_max - y_min) / (max_ticks - 1)
 
-                # Step 2: round step to nearest 1, 2, 5 * 10^n
-                magnitude = 10 ** math.floor(math.log10(raw_step))
-                residual = raw_step / magnitude
+        # Step 2: round step to nearest 1, 2, 5 * 10^n
+        magnitude = 10 ** math.floor(math.log10(raw_step))
+        residual = raw_step / magnitude
 
-                if residual <= 1:
-                    nice_step = 1 * magnitude
-                elif residual <= 2:
-                    nice_step = 2 * magnitude
-                elif residual <= 5:
-                    nice_step = 5 * magnitude
-                else:
-                    nice_step = 10 * magnitude
+        if residual <= 1:
+            nice_step = 1 * magnitude
+        elif residual <= 2:
+            nice_step = 2 * magnitude
+        elif residual <= 5:
+            nice_step = 5 * magnitude
+        else:
+            nice_step = 10 * magnitude
 
-                # Step 3: compute nice min and max ticks
-                nice_min = math.floor(y_min / nice_step) * nice_step
-                nice_max = math.ceil(y_max / nice_step) * nice_step
+        # Step 3: compute nice min and max ticks
+        nice_min = math.floor(y_min / nice_step) * nice_step
+        nice_max = math.ceil(y_max / nice_step) * nice_step
 
-                # Step 4: generate ticks
-                ticks = []
-                current = nice_min
-                while current <= nice_max + 1e-8:  # small epsilon for floating point
-                    ticks.append(round(current, 10))
-                    current += nice_step
+        # Step 4: generate ticks
+        ticks = []
+        current = nice_min
+        while current <= nice_max + 1e-8:  # small epsilon for floating point
+            ticks.append(round(current, 10))
+            current += nice_step
 
-                # Step 5: limit number of ticks
-                if len(ticks) > max_ticks:
-                    # pick evenly spaced subset including first and last
-                    step = len(ticks) / (max_ticks - 1)
-                    ticks = [ticks[round(i*step)] for i in range(max_ticks)]
+        # Step 5: limit number of ticks
+        if len(ticks) > max_ticks:
+            # pick evenly spaced subset including first and last
+            step = len(ticks) / (max_ticks - 1)
+            ticks = [ticks[round(i * step)] for i in range(max_ticks)]
 
-                return ticks
-
-            
+        return ticks
